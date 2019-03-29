4ffd5d17e503bac92c2f25392e6eb435713ae06e
Grzegorz Nawrocki <nawrocki@Ponto>
2019-03-29 15:50:35 -0400

    Determines cluster size distribution

--- oj.py
+++ oj.py
@@ -2,54 +2,9 @@
 import mdtraj as md
 import numpy as np
 
-### xyz of two atoms
-def dist(c1,c2):
- return np.sqrt(np.sum((c1-c2)**2))
-
 traj=md.load('../cg/sys.dcd',top='../cg/sys.pdb')
-#traj=md.load('../aa/sys.dcd',top='../aa/sys.pdb')
 
-print(traj)
-
-print('How many atoms?    %s' % traj.n_atoms)
-print('How many residues? %s' % traj.n_residues)
-print('What is the box size in x? %s' % traj.unitcell_lengths[0][0])	### [nm]
-
-frame_ndx=2 	### from zero
-atom_ndx=10	### from zero
-
-print('x: %s\ty: %s\tz: %s' % tuple(traj.xyz[frame_ndx, atom_ndx,:]))	### [nm]
-
-topology = traj.topology
-print(topology)
-
-
-
-print('Fifth atom: %s' % topology.atom(4))
-print('All atoms: %s' % [atom for atom in topology.atoms])
-
-print('Second residue: %s' % traj.topology.residue(1))
-print('All residues: %s' % [residue for residue in traj.topology.residues])
-
-atom = topology.atom(10)
-print('''Hi! I am the %sth atom, and my name is %s. 
-I am a %s atom with %s bonds. 
-I am part of an %s residue.''' % ( atom.index, atom.name, atom.element.name, atom.n_bonds, atom.residue.name))
-
-print([atom.index for atom in topology.atoms if atom.element.symbol is 'CGB' and atom.is_sidechain])
-
-print([residue for residue in topology.chain(0).residues if residue.index % 2 == 0])
-
-
-print(topology.select('resid 1 to 2'))
-print(topology.select('name N and backbone'))
-
-selection = topology.select_expression('name CGB and resid 1 to 5')
-print(selection)
-
-###############################
-
-cn=-1
+pn=-1	### pair number
 c=[]
 
 frame=0
@@ -85,8 +40,8 @@ for r1 in range(0,traj.n_residues-1):	### loops over all protein pairs
 #  dxyz=np.sqrt(dx**2+dy**2+dz**2)	### closest distance
   dxyz2=dx**2+dy**2+dz**2
 
-  rn1=topology.atom(r1).name
-  rn2=topology.atom(r2).name
+  rn1=traj.topology.atom(r1).name
+  rn2=traj.topology.atom(r2).name
 
   if(rn1=="CGA"):
    rc1=1.81273
@@ -119,15 +74,42 @@ for r1 in range(0,traj.n_residues-1):	### loops over all protein pairs
 #  print(r1+1,rn1,"-",r2+1,rn2,rc,dxyz)
 
   if(dxyz2<rc2):
-   print(frame+1,r1+1,r2+1)
+   print("fr:",frame,"pair:",r1,"-",r2)
 
-   cn=cn+1
-   c.append([cn,r1])
-   c.append([cn,r2])
+   pn=pn+1
+
+   c1=-1
+   c2=-1
+
+   if(pn==0):					### first pair
+    c.append([r1,r2])
+   else:
+    for i in range(0,len(c)):			### compare with existing clusters
+     if(r1 in c[i]):
+      c1=i
+     if(r2 in c[i]):
+      c2=i
+    if((c1==-1)&(c2==-1)):			### doesn't appear in any - add new
+     c.append([r1,r2])
+    elif((c1!=-1)&(c2!=-1)&(c1!=c2)):		### appears in two different
+     c.append(c[c1]+c[c2])
+     if(c1>c2):
+      del c[c1]
+      del c[c2]
+     else:
+      del c[c2]
+      del c[c1]
+    elif((c1!=-1)&(c2==-1)):				### appears in one
+     c[c1].append(r2)
+    elif((c2!=-1)&(c1==-1)):				### appears in one
+     c[c2].append(r1)
+     
+   print("clusters:",c)
+   print("cluster sizes:",len(c))
+
+for m in range(0,traj.n_atoms):			### add monomers
+ if(not any(m in i for i in c)):
+  c.append([m])
 
 print(c)
 
-c=[]
-
-#print("distance:",np.sqrt(np.sum((traj.xyz[frame,r1,:]-traj.xyz[frame,r2,:])**2)))
-#print("distance:",dist(traj.xyz[frame,r1,:],traj.xyz[frame,r2,:]))
