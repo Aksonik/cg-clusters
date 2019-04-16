5fe6035c2d3bdc2cbb6b4c73a81e938e315aee1e
Grzegorz Nawrocki <nawrocki@Ponto>
2019-04-16 17:26:22 -0400

    Number of contacts uncertainty.

--- contacts_plt.py
+++ contacts_plt.py
@@ -20,27 +20,31 @@ ax=plt.subplot(1,1,1)
 
 plt.axis('off')
 
-tab={}
+val={}
+err={}
 labx=[]
 laby=[]
 
 for line in fi:
  w=line.split()
 
- tab[w[0],w[1]]=w[2]
+ val[w[0],w[1]]=w[2]
+ err[w[0],w[1]]=w[4]
 
  if w[0] not in labx:
   labx.append(w[0])
  if w[1] not in laby:
   laby.append(w[1])
 
-mat=numpy.zeros((len(labx),len(laby)))
+valmat=numpy.zeros((len(labx),len(laby)))
+errmat=numpy.zeros((len(labx),len(laby)))
 
 for matx in range(0,len(labx)):
  for maty in range(0,len(laby)):
 
-  if (labx[matx],laby[maty]) in tab:
-   mat[matx,maty]=tab[labx[matx],laby[maty]]
+  if (labx[matx],laby[maty]) in val:
+   valmat[matx,maty]=val[labx[matx],laby[maty]]
+   errmat[matx,maty]=err[labx[matx],laby[maty]]
 
 ### plot label x
 
@@ -72,8 +76,10 @@ for matx in range(0,len(labx)):
 
   plot([0,10],[maty+1.5,maty+1.5],color=lc,lw=linew)	### horizontal line
 
-  if (labx[matx],laby[maty]) in tab:	### leave empty if the matrix is symmetric
-   ax.text(matx+2,len(laby)-maty,mat[matx,maty],ha="center",va="center",fontsize=fs)
+  if (labx[matx],laby[maty]) in val:	### leave empty if the matrix is symmetric
+   ax.text(matx+2,len(laby)-maty,
+   str(valmat[matx,maty])+r" $\pm$ "+str(errmat[matx,maty]),
+   ha="center",va="center",fontsize=fs)
 
 xlim(0.5,len(labx)+1.5)
 ylim(0.5,len(laby)+1.5)
